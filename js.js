be9fdc83cfda0b8237022f68a3c95b2d77190011
mehedi556 <mehe556di@gmail.com>
2022-08-24 18:20:51 +0600

    adding js functions

--- js.js
+++ js.js
@@ -1,24 +1,37 @@
-const cartArray = [];
+const palyersArray = [];
+
+function display(playersCart){
+console.log(playersCart);
+const tableBody = document.getElementById('players-cart');
+tableBody.innerHTML = '';
+for(let i = 0; i < playersCart.length; i++){
+    // console.log(playersCart[i].playerName);
+    const name = playersCart[i].playerName;
+
+const tr = document.createElement("tr");
+tr.innerHTML = `
+<th>${i + 1}</th>
+<td>${name}</td>
+`;
+tableBody.appendChild(tr);
+}
+}
 
 function addToCart(element){
-    
-    // console.log(element.parentNode.parentNode.children[0].innerText) 
-const playerName = element.parentNode.parentNode.children[0].innerText;
+    // console.log(element.parentNode.parentNode.children[0].innerText);
+    const playerName = element.parentNode.parentNode.children[0].innerText;
+    // console.log(playerName);
+    const playersObj = {
+        playerName:playerName
+    }
+palyersArray.push(playersObj)
+//     console.log(palyersArray);
+// console.log(palyersArray.length);
 
-const playerObj = {
-    playerName:playerName
-}
-cartArray.push(playerObj);
-// console.log(cartArray);
-document.getElementById('totalPlayer').innerText = cartArray.innerText;
+document.getElementById('total-added-player').innerText = palyersArray.length;
 
-}
-
-const players = document.getElementsByClassName('player');
-for(const player of players){
-
-player.addEventListener('click' , function(event){
-    event.target.parentNode
-})
+display(palyersArray)
 
 }
+
+
